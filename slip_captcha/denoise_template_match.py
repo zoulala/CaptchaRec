8799de85ef6eb822f26d5a1141843c1e00220a38
zoulingwei <zoulingwei@cyou-inc.com>
2019-03-25 14:27:07 +0800

    fix:  slip_captcha/,add test_api.py

--- slip_captcha/denoise_template_match.py
+++ slip_captcha/denoise_template_match.py
@@ -1,7 +1,9 @@
 
 import os
 import cv2
+import base64
 import numpy as np
+from io import BytesIO
 from PIL import Image
 from flask import Flask,request,jsonify
 
@@ -30,71 +32,67 @@ def reverse_img(img):
                 img[i][j] = 255
     return img
 
-def gen_big_img(filename):
-    img = Image.open(filename)
-    img.show()
-    img = img.convert('L')
+
+def base64_to_image(png_str):
+    png_bytes = base64.b64decode(png_str)
+    png_img = Image.open(BytesIO(png_bytes))
+    return png_img
+
+
+def gen_big_img(image):
+    img = image.convert('L')
     nimg = np.array(img)
     bimg = bi_img(nimg)
 
     big_img = Image.fromarray(bimg, 'L')
-    big_img.show()
+    # big_img.show()
     # mathe = np.array(big_img)
     return bimg
 
-def gen_lit_img(filename):
-    img = Image.open(filename)
-    n,m = img.size
+def gen_lit_img(image):
+    n,m = image.size
     # img.show()
-    l_img = img.convert('L')
+    l_img = image.convert('L')
     nimg = np.array(l_img)
     bimg = bi_img(nimg)
     img_2 = Image.fromarray(bimg, 'L')
     # img_2.show()
     # 分离通道
-    r, g, b, a = img.split()
+    r, g, b, a = image.split()
     aa = np.array(a)
     aa = reverse_img(aa)
     va = Image.fromarray(aa, 'L')
     # 粘贴
     new_img = Image.new("L", (n, m), (255,))  # RGBA的真色模式
     new_img.paste(img_2, (0, 0), mask=va)
-    new_img.show()
+    # new_img.show()
     temp = np.array(new_img)
     return temp
 
 
 @app.route('/v2/cyou/captcha/toutiao', methods=['POST'])
 def slip_captcha():
-    data = request.get_json()
+    try:
+        data = request.get_json()
 
-    big_img = data['big_img']
-    lit_img = data['lit_img']
+        big_data = data.get('background')
+        lit_data = data.get('template')
 
-    big = gen_big_img(filename0)
-    temp = gen_lit_img(filename1)
+        big_image = base64_to_image(big_data)
+        lit_image = base64_to_image(lit_data)
 
-    methods = [cv2.TM_SQDIFF_NORMED, cv2.TM_CCORR_NORMED, cv2.TM_CCOEFF_NORMED]
-    result = cv2.matchTemplate(big,temp,methods[1])
-    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
-    print(max_loc, max_val)
+        big = gen_big_img(big_image)
+        temp = gen_lit_img(lit_image)
 
-    return jsonify({'code': code, 'message': message})
+        methods = [cv2.TM_SQDIFF_NORMED, cv2.TM_CCORR_NORMED, cv2.TM_CCOEFF_NORMED]
+        result = cv2.matchTemplate(big,temp,methods[1])
+        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+        print(max_loc, max_val)
+
+        return jsonify({'code': 1, 'message': 'ok', 'data': max_loc[0]})
+    except:
+        return jsonify({'code': 0, 'message': 'error','data':None})
 
 
 if __name__=="__main__":
-    path = r"C:\Users\zoulingwei\Desktop\captchaRec\RR"
-
-    file_list = os.listdir(path)
-
-    filename0 = os.path.join(path,file_list[2])
-    filename1 = os.path.join(path,file_list[3])
-
-    big = gen_big_img(filename0)
-    temp = gen_lit_img(filename1)
-
-
-    methods = [cv2.TM_SQDIFF_NORMED, cv2.TM_CCORR_NORMED, cv2.TM_CCOEFF_NORMED]
-    result = cv2.matchTemplate(big,temp,methods[1])
-    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
-    print(max_loc, max_val)
+    app.run(host='10.12.0.54', port=1313)
